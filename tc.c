1aa7fb220621f2cd9157f2467bbc1cb89f794fde
Randy Li <randy@soulik.info>
2016-05-27 18:02:10 +0800

    tc: fix system call, mac filter and few typo
    You can't return an value in do{}while(0).
    The MAC address in u32 now is correct. With wireshark, you would find
    the DSAO, SSAO, cntl, and org code won't be there.

--- tc.c
+++ tc.c
@@ -5,14 +5,14 @@
 #define EXEC_STRING(template, args...) \
 	do {uint8_t buf[200]; \
 	snprintf(buf, sizeof(buf), template, ## args); \
-	return system(buf); \
+	system(buf); \
 	} \
 	while(0)
 
-uint8_t tc_init(void *data)
+int8_t tc_init(void *data)
 {
 	struct tc_if_config *config = (struct tc_if_config *)data;
-	uint16_t rate;
+	uint32_t rate;
 	uint16_t netmask;
 	uint16_t subnet;
 	uint8_t *subnet4_addr;
@@ -23,10 +23,10 @@ uint8_t tc_init(void *data)
 	subnet = network_ipv4_subnet(config->in_if);
 	subnet += 2;
 
-	if (0 != network_ipv4_prefix(config->in_if, subnet4_addr))
+	if (!network_ipv4_prefix(config->in_if, &subnet4_addr))
 		return -1;
 
-	EXEC_STRING("tc tc qdisc del dev %s root", config->out_if);
+	EXEC_STRING("tc qdisc del dev %s root", config->out_if);
 	EXEC_STRING("tc qdisc add dev %s root handle 1 hfsc default %d",
 		config->out_if, subnet);
 
@@ -43,7 +43,7 @@ uint8_t tc_init(void *data)
 	EXEC_STRING("tc filter add dev %s parent 1: protocol arp prio 2 u32 match u32 0 0 flowid 1:1",
 		config->out_if);
 
-	EXEC_STRING("tc tc qdisc del dev %s root", config->in_if);
+	EXEC_STRING("tc qdisc del dev %s root", config->in_if);
 	EXEC_STRING("tc qdisc add dev %s root handle 1 hfsc default %d",
 		config->in_if, subnet);
 
@@ -52,36 +52,42 @@ uint8_t tc_init(void *data)
 		config->in_if, subnet, rate, rate);
 	EXEC_STRING("tc qdisc add dev %s parent 1:%d handle %d: fq_codel",
 		config->in_if, subnet, subnet);
-	EXEC_STRING("tc filter add dev %s parent %d: handle %d protocol all flow map key dst-%s divisor 256",
-		config->in_if, subnet, subnet4_addr);
+	EXEC_STRING("tc filter add dev %s parent %d: handle %d protocol all flow map key dst addend -%s divisor 256",
+		config->in_if, subnet, subnet, subnet4_addr);
 
 	EXEC_STRING("tc class add dev %s  parent 1: classid 1:1 hfsc sc rate %dkbit ul rate %dkbit",
 		config->in_if, config->download_rate, config->download_rate);
 	EXEC_STRING("tc filter add dev %s parent 1: protocol arp prio 1 u32 match u32 0 0 flowid 1:1",
 		config->in_if);
 
+	return 0;
+
 }
 
-int8_t tc_add_expection(void *data)
+int8_t tc_add_exception(void *data)
 {
 	struct tc_if_config *config = (struct tc_if_config *)data;
 
 	struct ether_addr *mac;
-	struct xcostc_mac *ptr;
+	struct xcostc_mac *ptr, *tmp;
+	struct list_head *head;
 
 	
 	/* FIXME this way only work for the download traffic control,
 	 * as we do that in the lan side
 	 */
+	head = &config->white_list;
 
-	list_for_each_entry(ptr, config->ip4_white_list, list) {
+	list_for_each_entry_safe(ptr, tmp, head, list) {
 		mac = &(ptr->mac);
 		EXEC_STRING(
-		"tc filter add dev %s parent 1: protocol ip prio 2 u32 match u16 0x0800 0xFFFF at -2  \
+		"tc filter add dev %s parent 1: protocol ip prio 3 u32 match u16 0x0800 0xFFFF at -2  \
 		match u32 0x%X%X%X%X 0xFFFFFFFF at -12 match u16 0x%X%X 0xFFFF at -14 flowid 1:1",
-		config->in_if, mac->ether_addr_octet[0], mac->ether_addr_octet[1],
-		mac->ether_addr_octet[2], mac->ether_addr_octet[3], 
-		mac->ether_addr_octet[4], mac->ether_addr_octet[5]);
+		config->in_if, mac->ether_addr_octet[2], mac->ether_addr_octet[3],
+		mac->ether_addr_octet[4], mac->ether_addr_octet[5], 
+		mac->ether_addr_octet[0], mac->ether_addr_octet[1]);
 	}
+
+	return 0;
 
 }
--- tc.h
+++ tc.h
@@ -2,4 +2,6 @@
 #define _TC_H_
 #include "common.h"
 
+int8_t tc_init(void *data);
+int8_t tc_add_exception(void *data);
 #endif
